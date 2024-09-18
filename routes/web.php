1a9b4b61647f63880ea9d1155d84e6c822996213
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2024-09-18 16:59:31 +0900

    #4 ヘッダー、フッターを作成など

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/create', function () {
+    return view('create');
+})->middleware(['auth', 'verified'])->name('create');
+
+Route::post('/users/store', [UsersController::class, 'store'])->name('users.store');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
